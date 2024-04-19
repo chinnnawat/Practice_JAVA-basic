66f7cdd1b0e30eb1d8836e16559cce6d47db84e9
chinnnawat <chin3896@gmail.com>
2024-04-19 21:16:26 +0700

    CRUD

--- IDE/JDBCCourse/src/DemoJdbc.java
+++ IDE/JDBCCourse/src/DemoJdbc.java
@@ -17,22 +17,36 @@ public class DemoJdbc {
         String pass = "1202";
 
         /* SQL */
-        String sql = "select * from student";
-
+/*        String sql = "select * from student";     /* Select User */
+//        String sql = "insert into student values (3, 'Jane', 48 )" ;        /* Create User */
+//        String sql = "update student set sname='Max' where sid=3 ";     /* Update info User by sid */
+        String sql = "delete from student where sid=3";     /* Delete User */
+//        *************************************************************************************************************************
 //        Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("Connected Database");
         Statement st = con.createStatement();
 
-        ResultSet rs = st.executeQuery(sql);
+        /* Get Data from Database */
+/*        while (rs.next()){
+//            System.out.print(rs.getInt(1) + " : ");
+//            System.out.print(rs.getString(2) + " : ");
+//            System.out.println(rs.getInt(3));
+//        }
+ */
 
-//        Get Data from Database
-        while (rs.next()){
-            System.out.print(rs.getInt(1) + " : ");
-            System.out.print(rs.getString(2) + " : ");
-            System.out.println(rs.getInt(3));
-        }
-        System.out.println("Connection Closed");
+        /* Create Student
+        boolean status = st.execute(sql);
+        System.out.println(status);
+        */
+
+        /* Update info User */
+//        st.execute(sql);
+
+        /* Delete User */
+        st.execute(sql);
+
+
         con.close();
 
     }
