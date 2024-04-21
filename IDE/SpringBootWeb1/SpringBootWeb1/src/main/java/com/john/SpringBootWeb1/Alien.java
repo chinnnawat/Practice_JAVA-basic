79a26daa1a059f02d46805d6e5784ff5c823f481
chinnnawat <chin3896@gmail.com>
2024-04-21 17:09:18 +0700

    modelAttribute

---
+++ IDE/SpringBootWeb1/SpringBootWeb1/src/main/java/com/john/SpringBootWeb1/Alien.java
@@ -0,0 +1,31 @@
+package com.john.SpringBootWeb1;
+
+public class Alien {
+
+    private int id;
+    private  String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- IDE/SpringBootWeb1/SpringBootWeb1/src/main/java/com/john/SpringBootWeb1/HomeController.java
+++ IDE/SpringBootWeb1/SpringBootWeb1/src/main/java/com/john/SpringBootWeb1/HomeController.java
@@ -1,7 +1,12 @@
 package com.john.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -9,6 +14,29 @@ public class HomeController {
     @RequestMapping("/")
     public String home(){
         System.out.println("Home method Called");
-        return "index.jsp";
+        return "index";
+    }
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
+        int result = num1 + num2;
+        mv.addObject("result", result);
+        mv.setViewName("result");
+        return mv;
+    }
+
+    @RequestMapping("addAlien")
+    public ModelAndView addAlien(@RequestParam("aid") int aid, @RequestParam("aname") String aname, ModelAndView mv){
+        Alien alien = new Alien();
+
+        /* setID, setName */
+        alien.setId(aid);
+        alien.setName(aname);
+
+        mv.addObject("alien", alien);
+
+        /* result.jsp */
+        mv.setViewName("result");
+
+        return mv;
     }
 }
