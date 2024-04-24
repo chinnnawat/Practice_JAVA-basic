232f30cfe14e5420434bcad7fa843f21faeb3bb7
chinnnawat <chin3896@gmail.com>
2024-04-24 13:04:49 +0700

    setUp for JWT

--- IDE/spring-sec-demo/spring-sec-demo/src/main/java/com/john/springsecdemo/config/SecurityConfig.java
+++ IDE/spring-sec-demo/spring-sec-demo/src/main/java/com/john/springsecdemo/config/SecurityConfig.java
@@ -1,18 +1,22 @@
 package com.john.springsecdemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,42 +25,32 @@ public class SecurityConfig {
     @Autowired
     private UserDetailsService userDetailsService;
 
+
     @Bean
-    /* Authentication Provider */
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         return provider;
     }
+
+
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("register")
+                        .permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
-
-    /* @Bean = สร้างและกำหนดค่า SecurityFilterChain ซึ่งเป็นตัวจัดการการรักษาความปลอดภัยของแอปพลิเคชัน */
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        /* SignIn */
-//        http.csrf(customizer -> customizer.disable())
-//                /*กำหนดว่าทุก HTTP request จะต้องมีการ authenticate ก่อนเข้าถึง*/
-//                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-//
-//                .httpBasic(Customizer.withDefaults())
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
-//
-//        /* คืนค่า SecurityFilterChain ที่กำหนดค่าแล้วให้ Spring Security นำไปใช้งาน */
-//        return http.build();
-//
-//    }
 
 }
 
--- IDE/spring-sec-demo/spring-sec-demo/src/main/java/com/john/springsecdemo/model/User.java
+++ IDE/spring-sec-demo/spring-sec-demo/src/main/java/com/john/springsecdemo/model/User.java
@@ -1,8 +1,6 @@
 package com.john.springsecdemo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +11,7 @@ import lombok.NoArgsConstructor;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
